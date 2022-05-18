36ba6eac6ce0ec882db6447d7b527f6326300c6b
thisisjona <thisisjona@gmail.com>
2022-05-17 21:34:12 -0500

    dynamically created timeslots

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,27 @@
 $(document).ready(function() {
     var currentDate = moment().format('dddd, MMMM Do');
+    var currentTime = moment().format('HH');
+    var container = $('.container');
 
+    //display current date at header section
     $('#currentDay').append(currentDate);
+    console.log(currentTime);
+
+    // Create Timeslot elements
+
+var row = $('<div class="row time-slot"></div>');
+var hourCol = $('<div class= "col hour"></div>');
+
+var column2 = $('<div class= "col-6"></div>');
+var inputField = $('<textarea class="form-control textarea"></textarea>');
+var saveBtn = $('<button type= "button" class="btn col-md-10 saveBtn"></button>');
+// saveBtn.attr("id", i);
+
+var column3 = $('<div class="col"></div>');
+column3.append(saveBtn);
+
+container.append(row);
+
+row.append(hourCol, column2, column3);
 });
+
