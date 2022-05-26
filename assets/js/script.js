14fff2f6289a051eb80d73cfb83dbe857ff8a445
thisisjona <thisisjona@gmail.com>
2022-05-26 17:55:29 -0500

    render input field color accoreding to current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,13 +8,25 @@ $(document).ready(function() {
     $('#currentDay').append(currentDate);
     console.log(currentTime);
 
-    // Dynamically Create Timeslot Elements
+    //main loop
     for (let i = 0; i < 9 ; i++){
+//Change text input field color according to past present or future
+var color
+if(currentTime === i + 9) {
+    color = 'present';
+}else if(currentTime < i + 9) {
+    color = 'future';
+}else {
+    color = 'past';
+};
+
+// Dynamically Create Timeslot Elements
 var row = $('<div class="row time-slot"></div>');
 var hourCol = $('<div class= "col hour"></div>');
 
 var taskCol = $('<div class= "col-6"></div>');
 var inputField = $('<textarea class="form-control textarea"></textarea>');
+inputField.addClass(color);
 var saveBtn = $('<button type= "button" class="btn col-md-10 saveBtn"></button>');
 saveBtn.html('SAVE TASK')
 // saveBtn.attr("id", i);
@@ -22,10 +34,20 @@ saveBtn.html('SAVE TASK')
 var saveCol = $('<div class="col"></div>');
 saveCol.append(saveBtn);
 taskCol.append(inputField);
-hourCol.html('yooooo');
+hourCol.text(setMeridian(i + 9));
 
 container.append(row);
 row.append(hourCol, taskCol, saveCol);
+};
+
+//set AM vs PM times
+function setMeridian(hour) {
+    if(hour > 12){
+        var timeText =  hour -12;
+        return timeText + 'pm';
+    }else {
+        return hour + 'am';
+    }
 };
 
 });
